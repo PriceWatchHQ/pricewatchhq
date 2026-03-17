894438eb3147d66b2553dd5a348e05c77e77a701
Logan <logan@pricewatchhq.com>
2026-03-16 19:46:22 -0500

    Use DB_DIR env var for persistent volume storage

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const DB_DIR = join(__dirname, '..', 'data');
+const DB_DIR = process.env.DB_DIR || join(__dirname, '..', 'data');
 const DB_PATH = join(DB_DIR, 'pricewatchhq.db');
 
 if (!existsSync(DB_DIR)) {
